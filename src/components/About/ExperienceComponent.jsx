32b4ceeff4c0d0a61983658d60cb8d62d72f9d4c
Faizan Anwar Khan <139392689+Faizan6100-amu@users.noreply.github.com>
2025-02-20 01:15:40 +0530

    Update ExperienceComponent.jsx
    Formatting updated

--- src/components/About/ExperienceComponent.jsx
+++ src/components/About/ExperienceComponent.jsx
@@ -4,37 +4,27 @@ const EducationComponent = () => {
       <ul className="divide-y divide-gray-300 dark:divide-gray-700">
         <li className="py-4">
           <span className="text-sm text-slate-500 dark:text-slate-400">
-          Sept 1, 2024 - Dec 30, 2024
+          Sept 1, 2024 - Dec 30, 2024  | New Delhi, India 
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Associate Web Developer, Indian Institute of Sustainable Development (IISD)
+          Associate Web Developer | <span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Indian Institute of Sustainable Development (IISD)
           </p>
-          <span className="text-sm text-slate-500 dark:text-slate-400">
-          New Delhi, India
-          </span>
         </li>
         <li className="py-4">
           <span className="text-sm text-slate-500 dark:text-slate-400">
-          Jan 24, 2024 - July 24, 2024
+          Jan 24, 2024 - July 24, 2024  | Aligarh, India
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Web Developer, NEP-Cell (Aligarh Muslim University)
+          Web Developer | <span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> NEP-Cell (Aligarh Muslim University)
           </p>
-          <span className="text-sm text-slate-500 dark:text-slate-400">
-          Aligarh, India
-          </span>
         </li>
         <li className="py-4">
           <span className="text-sm text-slate-500 dark:text-slate-400">
-          Nov 10, 2022 - March 10, 2023
+          Nov 10, 2022 - March 10, 2023  | Remote, India
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Programming Trainee, Coding Ninjas
+          Programming Trainee | <span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Coding Ninjas
           </p>
-          
-          <span className="text-sm text-slate-500 dark:text-slate-400">
-          Remote, India
-          </span>
         </li>
       </ul>
     </div>
