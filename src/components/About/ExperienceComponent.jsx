000475bd85db8e1b79949b98bc51b5ca97e92fac
Faizan Anwar Khan <139392689+Faizan6100-amu@users.noreply.github.com>
2025-02-20 01:23:32 +0530

    Update ExperienceComponent.jsx

--- src/components/About/ExperienceComponent.jsx
+++ src/components/About/ExperienceComponent.jsx
@@ -7,7 +7,7 @@ const EducationComponent = () => {
           Sept 1, 2024 - Dec 30, 2024  | New Delhi, India 
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Associate Web Developer | <span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Indian Institute of Sustainable Development (IISD)
+          Associate Web Developer <br/><span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Indian Institute of Sustainable Development (IISD)
           </p>
         </li>
         <li className="py-4">
@@ -15,7 +15,7 @@ const EducationComponent = () => {
           Jan 24, 2024 - July 24, 2024  | Aligarh, India
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Web Developer | <span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> NEP-Cell (Aligarh Muslim University)
+          Web Developer <br/><span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> NEP-Cell (Aligarh Muslim University)
           </p>
         </li>
         <li className="py-4">
@@ -23,7 +23,7 @@ const EducationComponent = () => {
           Nov 10, 2022 - March 10, 2023  | Remote, India
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Programming Trainee | <span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Coding Ninjas
+          Programming Trainee <br/><span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Coding Ninjas
           </p>
         </li>
       </ul>
