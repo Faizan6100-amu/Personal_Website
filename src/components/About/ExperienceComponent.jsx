6ec51538f8d4d2cdbe44408f00e0e788027e7be7
Faizan Anwar Khan <139392689+Faizan6100-amu@users.noreply.github.com>
2025-10-02 22:05:01 +0530

    Update job experience details

--- src/components/About/ExperienceComponent.jsx
+++ src/components/About/ExperienceComponent.jsx
@@ -4,10 +4,18 @@ const EducationComponent = () => {
       <ul className="divide-y divide-gray-300 dark:divide-gray-700">
         <li className="py-4">
           <span className="text-sm text-slate-500 dark:text-slate-400">
-          Sept 1, 2024 - Dec 30, 2024  | New Delhi, India 
+          July 23, 2025 - Current  | Bengaluru, India 
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Associate Web Developer <br/><span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Indian Institute of Sustainable Development (IISD)
+          Assistant Professor <br/><span className="text-sm text-slate-500 dark:text-slate-400">School of CSE, Presidency University</span>
+          </p>
+        </li>
+        <li className="py-4">
+          <span className="text-sm text-slate-500 dark:text-slate-400">
+          Sept 1, 2024 - Jan 31, 2025  | New Delhi, India 
+          </span>
+          <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
+          Associate Software Developer <br/><span className="text-sm text-slate-500 dark:text-slate-400">Indian Institute of Sustainable Development(IISD)</span>
           </p>
         </li>
         <li className="py-4">
@@ -15,15 +23,15 @@ const EducationComponent = () => {
           Jan 24, 2024 - July 24, 2024  | Aligarh, India
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Web Developer <br/><span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> NEP-Cell (Aligarh Muslim University)
+          Software Developer <br/><span className="text-sm text-slate-500 dark:text-slate-400">NEP-Cell (Aligarh Muslim University)</span>
           </p>
         </li>
         <li className="py-4">
           <span className="text-sm text-slate-500 dark:text-slate-400">
-          Nov 10, 2022 - March 10, 2023  | Remote, India
+          Nov 01, 2022 - March 30, 2023  | Remote, India
           </span>
           <p className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-          Programming Trainee <br/><span className="text-sm text-slate-500 dark:text-slate-400">Company:</span> Coding Ninjas
+          Programming Trainee <br/><span className="text-sm text-slate-500 dark:text-slate-400">Coding Ninjas Pvt.Ltd.</span> 
           </p>
         </li>
       </ul>
